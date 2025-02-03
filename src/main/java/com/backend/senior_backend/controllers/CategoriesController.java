be1bbf8c1deb8239fb246b511b224d238ee4216f
Omar Shaqaqi <shaqaqio@gmail.com>
2025-02-03 19:15:38 +0300

    Adding @RestController Annotaion for all controllers

--- src/main/java/com/backend/senior_backend/controllers/CategoriesController.java
+++ src/main/java/com/backend/senior_backend/controllers/CategoriesController.java
@@ -1,5 +1,8 @@
 package com.backend.senior_backend.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoriesController {
 
 }
--- src/main/java/com/backend/senior_backend/controllers/ParticipantsController.java
+++ src/main/java/com/backend/senior_backend/controllers/ParticipantsController.java
@@ -1,5 +1,10 @@
 package com.backend.senior_backend.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.Resource;
+
+@RestController
 public class ParticipantsController {
 
 }
