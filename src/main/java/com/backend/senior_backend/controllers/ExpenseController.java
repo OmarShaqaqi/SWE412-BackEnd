490f96c5f41f0f94fec3c1ceb23f8738a6a517ae
Omar Shaqaqi <shaqaqio@gmail.com>
2025-05-04 02:49:52 +0300

    expenses deleting

--- src/main/java/com/backend/senior_backend/controllers/ExpenseController.java
+++ src/main/java/com/backend/senior_backend/controllers/ExpenseController.java
@@ -72,7 +72,7 @@ public class ExpenseController {
         return ResponseEntity.ok(expenses);
     }
 
-    @DeleteMapping("/delete")
+    @GetMapping("/delete")
     public ResponseEntity<String> deleteExpense(@RequestParam Long expenseId) {
         String response = expenseService.deleteExpense(expenseId);
         return ResponseEntity.ok(response);
