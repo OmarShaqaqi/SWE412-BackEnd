fa845e2fdb7b52a48600ca04d26463c10dd7b7a7
Omar Shaqaqi <shaqaqio@gmail.com>
2025-05-05 20:53:04 +0300

    participants images

--- src/main/java/com/backend/senior_backend/dto/ParticipantExpenseDTO.java
+++ src/main/java/com/backend/senior_backend/dto/ParticipantExpenseDTO.java
@@ -1,41 +1,17 @@
 package com.backend.senior_backend.dto;
 
+
+
+@lombok.Data
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
 public class ParticipantExpenseDTO {
 
     private String phone;
     private double totalExpense;
     private boolean isLeader;
+    private String image;
 
-    // Constructor
-    public ParticipantExpenseDTO(String phone, double totalExpense, boolean isLeader) {
-        this.phone = phone;
-        this.totalExpense = totalExpense;
-        this.isLeader = isLeader;
-    }
-
-    // Getters and setters
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public double getTotalExpense() {
-        return totalExpense;
-    }
-
-    public void setTotalExpense(double totalExpense) {
-        this.totalExpense = totalExpense;
-    }
-
-    public boolean isLeader() {
-        return isLeader;
-    }
-
-    public void setLeader(boolean leader) {
-        isLeader = leader;
-    }
+    
 }
 
